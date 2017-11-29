1664f543048c713a6f0cf9e70e5a3459b0fdf92b
dperezarbues <dperezarbues@gmail.com>
2017-11-29 09:09:22 +0000

    PostgreSQL integration

--- src/main/java/com/example/demo/config/DatabaseConfig.java
+++ src/main/java/com/example/demo/config/DatabaseConfig.java
@@ -7,8 +7,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
-import org.springframework.orm.hibernate4.HibernateTransactionManager;
-import org.springframework.orm.hibernate4.LocalSessionFactoryBean;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import java.net.URI;
