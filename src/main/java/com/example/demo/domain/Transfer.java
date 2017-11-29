1e9602e2d28a0956bb41f557b27efce1305f578c
dperezarbues <dperezarbues@gmail.com>
2017-11-29 10:48:44 +0000

    PostgreSQL Integration

--- src/main/java/com/example/demo/domain/Transfer.java
+++ src/main/java/com/example/demo/domain/Transfer.java
@@ -12,6 +12,7 @@ import java.math.BigDecimal;
 import java.util.concurrent.atomic.AtomicLong;
 
 import javax.persistence.Entity;
+import javax.persistence.Table;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
