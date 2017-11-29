5750d28236369a7f32403efe643a8c1c2e1ce69d
dperezarbues <dperezarbues@gmail.com>
2017-11-29 08:43:49 +0000

    PostgreSQL integration

--- src/main/java/com/example/demo/config/DatabaseConfig.java
+++ src/main/java/com/example/demo/config/DatabaseConfig.java
@@ -1,11 +1,31 @@
 package com.example.demo.config;
 
+import org.apache.commons.dbcp.BasicDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
 @Configuration
+@ComponentScan(basePackages = "com.heroku.example")
 public class DatabaseConfig {
+
     @Bean
-    @Primary
-    @ConfigurationProperties(prefix = "spring.datasource")
-    public DataSource dataSource() {
-        return DataSourceBuilder.create().build();
+    public BasicDataSource dataSource() throws URISyntaxException {
+        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + dbUri.getPath();
+
+        BasicDataSource basicDataSource = new BasicDataSource();
+        basicDataSource.setUrl(dbUrl);
+        basicDataSource.setUsername(username);
+        basicDataSource.setPassword(password);
+
+        return basicDataSource;
     }
+
 }
